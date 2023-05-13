ec425a4ba5467f49ffe18344dbe8ed829725553b
kaline <mesquitabfkalinekaline@gmail.com>
2023-05-13 16:06:22 -0300

    update

---
+++ 3.lacosRepeticao.js
@@ -0,0 +1,9 @@
+//[] 1. Crie uma lista que recebe como argumentos o nome de um aluno - checked
+//2. Verifique se o aluno está presente na lista e retorne a média final que se encontra no mesmo índice.
+//3. Caso o aluno não esteja na lista, retorne uma mensagem indicando que o aluno não foi encontrado.
+
+
+seacrchList(studentName) {
+
+
+}
